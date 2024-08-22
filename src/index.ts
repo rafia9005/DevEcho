e3b278cc37eeeced55450018513f1ae633c5ffb8
rafia9005 <rafia9005@gmail.com>
2024-08-22 19:23:56 +0700

    npx prisma init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import r from "./routes/route";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api", r);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
---
+++ src/routes/route.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const r = Router();
+
+// routing
+
+export default r;
