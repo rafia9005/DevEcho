7af61371b8a94db80b17faa0a649f0f11d686bc4
rafia9005 <rafia9005@gmail.com>
2024-08-24 19:16:10 +0700

    add: pages ( register - login )

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </>
