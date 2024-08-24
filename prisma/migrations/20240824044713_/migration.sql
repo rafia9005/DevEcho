5468ca90d99ae7245846a1e43f2e58fdf1addcf1
rafia9005 <rafia9005@gmail.com>
2024-08-24 20:15:04 +0700

    change all

---
+++ prisma/migrations/20240824044713_/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `token` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `users` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Blog` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `token` ADD CONSTRAINT `token_users_fkey` FOREIGN KEY (`users`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Index() {
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
-}
-
